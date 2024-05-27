7ef3225ee42da5243f28103c6d2aba07c0c95018
Deyvid Rannyere <deyran@gmail.com>
2024-05-27 10:21:12 -0300

    Update

--- SGestorEscola/MainPage.xaml.cs
+++ SGestorEscola/MainPage.xaml.cs
@@ -1,9 +1,9 @@
-﻿namespace SGestorEscola
+﻿using SGestorEscola.Models;
+
+namespace SGestorEscola
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
@@ -11,14 +11,7 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            new Boletim().CreatePDF();
         }
     }
 
